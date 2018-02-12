a581cf521a7f22fc529923ed161075e523ea266c
Brudus <freelance@manuelschulze.com>
2018-02-12 20:55:29 +0100

    Initial commit

---
+++ WebViewTest/NetworkService.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkService.swift
+//  WebViewTest
+//
+//  Created by Manuel Schulze on 12.02.18.
+//  Copyright © 2018 Manuel Schulze. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class NetworkService {
+    static let shared = NetworkService()
+    
+    // Thread-safe
+    private init() {}
+    
+    func getFile(path: String, completion: @escaping() -> Void) {
+        let destination: DownloadRequest.DownloadFileDestination = {_, _ in
+            let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            print(path)
+            let fileURL = documentURL.appendingPathComponent(path)
+            
+            print(fileURL)
+            
+            return (fileURL, [.removePreviousFile, .createIntermediateDirectories])
+        }
+        
+        Alamofire.download("https://manuelschulze.com/" + path, to: destination)
+            .responseData { response in
+                if let error = response.result.error {
+                    print("Error: \(error.localizedDescription)")
+                }
+                
+               completion()
+        }
+    }
+}
---
+++ WebViewTest/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  WebViewTest
+//
+//  Created by Manuel Schulze on 09.02.18.
+//  Copyright © 2018 Manuel Schulze. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var loadingView: UIView!
+    @IBOutlet weak var testImageView: UIImageView!
+    
+    @IBAction func loadWebsite(_ sender: Any) {
+        loadingView.isHidden = false
+        
+        NetworkService.shared.getFile(path: "webview.html", completion: {
+            NetworkService.shared.getFile(path: "css/style.css", completion: {
+                NetworkService.shared.getFile(path: "images/square.jpg", completion: {
+                    self.loadingView.isHidden = true
+                    self.webView.isHidden = false
+                    self.fillInContent()
+                })
+            })
+        })
+    }
+    
+    private func fillInContent() {
+        let documentURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let fileUrl = documentURL.appendingPathComponent("webview.html")
+        
+        if FileManager.default.fileExists(atPath: fileUrl.path) {
+            if let data = FileManager.default.contents(atPath: fileUrl.path) {
+                self.webView.loadFileURL(fileUrl, allowingReadAccessTo: documentURL)
+                //self.webView.load(data, mimeType: "text/html", characterEncodingName: "utf8", baseURL: fileUrl)
+            }
+        }
+    }
+    
+}
+
